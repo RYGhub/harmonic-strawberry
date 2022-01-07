d31aa5d13d102e843977abc77765465625adc92f
Stefano Pigozzi <me@steffo.eu>
2022-01-07 18:33:28 +0100

    💥 list, edit, delete, create

--- src/cmdhandle.ts
+++ src/cmdhandle.ts
@@ -3,8 +3,10 @@ import {Snowflake} from "discord-api-types/v9"
 import {ApplicationCommandPermissions, Collection, CommandInteraction, Guild, GuildApplicationCommandPermissionData, Interaction, Permissions, TextChannel, Webhook} from "discord.js"
 import {ApplicationCommandPermissionTypes} from "discord.js/typings/enums"
 import {api as isapi} from "./impressive"
-import {GroupFull, WebhookFull, WebhookKind} from "./types"
+import {renderAchievement} from "./msgrender"
+import {AchievementFull, GroupFull, WebhookFull, WebhookKind} from "./types"
 import {rest as dsapi, client as dsclient} from "./discord"
+import exp = require("constants")
 
 
 const HANDLERS = {
@@ -20,7 +22,17 @@ const HANDLERS = {
 
 
 export async function handleCommand(interaction: CommandInteraction): Promise<void> {
-    await HANDLERS[interaction.commandName](interaction)
+    try {
+        await HANDLERS[interaction.commandName](interaction)
+    }
+    catch (e) {
+        if(e.response?.data?.reason) {
+            await interaction.reply(`:warning: ${e.response.data.reason}`)
+        }
+        else {
+            console.error(e)
+        }
+    }
 }
 
 async function findGuildGroup(interaction: Interaction): Promise<GroupFull> {
@@ -124,23 +136,104 @@ async function handleSetup(interaction: CommandInteraction): Promise<void> {
     const isWebhook = await setupImpressiveWebhook(dsWebhook)
     const permissions = await setupCommandPermissions(guild, interaction.options.get("role").role.id)
 
-    await interaction.reply(`:hammer: Set up achievements in channel <#${channel.id}> and enabled the other Strawberry commands!`)
+    await interaction.reply({
+        content: `:hammer: Set up achievements in channel <#${channel.id}> and enabled the other Strawberry commands!`,
+        ephemeral: true,
+    })
 }
 
 async function handleList(interaction: CommandInteraction): Promise<void> {
+    const response = await isapi.get<AchievementFull[]>("/api/achievement/v1/", {
+        params: {
+            group: interaction.guildId,
+        }
+    })
 
+    const achievements = response.data
+    const embeds = achievements.map(ach => renderAchievement(ach, true))
+
+    const expectedMessages = Math.ceil(embeds.length / 10)
+    for(let msgNo = 0; msgNo < expectedMessages; msgNo++) {
+        const msgEmbeds = embeds.slice(msgNo * 10, msgNo * 10 + 10)
+        if(msgNo === 0) {
+            await interaction.reply({
+                embeds: msgEmbeds,
+                ephemeral: true,
+            })
+        }
+        else {
+            await interaction.followUp({
+                embeds: msgEmbeds,
+                ephemeral: true,
+            })
+        }
+    }
 }
 
 async function handleCreate(interaction: CommandInteraction): Promise<void> {
+    const response = await isapi.post<AchievementFull>("/api/achievement/v1/", {
+        name: interaction.options.get("name").value,
+        description: interaction.options.get("description").value,
+        alloy: interaction.options.get("alloy").value,
+        secret: interaction.options.get("secret").value,
+        repeatable: interaction.options.get("repeatable").value ?? false,
+        crystal: interaction.options.get("crystal").value,
+    }, {
+        params: {
+            group: interaction.guildId
+        }
+    })
 
+    const achievement = response.data
+    await interaction.reply({
+        content: `:sparkles: Created achievement \`${achievement.crystal}\`!`,
+        embeds: [renderAchievement(achievement, true)],
+        ephemeral: true,
+    })
 }
 
 async function handleEdit(interaction: CommandInteraction): Promise<void> {
+    const crystal = interaction.options.get("crystal").value
+    const getResponse = await isapi.get<AchievementFull>(`/api/achievement/v1/${crystal}`, {
+        params: {
+            group: interaction.guildId
+        }
+    })
+    const getAchievement = getResponse.data
 
+    const putResponse = await isapi.put<AchievementFull>(`/api/achievement/v1/${crystal}`, {
+        name: interaction.options.get("name")?.value ?? getAchievement.name,
+        description: interaction.options.get("description")?.value ?? getAchievement.description,
+        alloy: interaction.options.get("alloy")?.value ?? getAchievement.alloy,
+        secret: interaction.options.get("secret")?.value ?? getAchievement.secret,
+        repeatable: interaction.options.get("repeatable")?.value ?? getAchievement.repeatable,
+        crystal: crystal,
+    }, {
+        params: {
+            group: interaction.guildId,
+        }
+    })
+
+    const putAchievement = putResponse.data
+    await interaction.reply({
+        content: `:pencil2: Edited achievement \`${getAchievement.crystal}\`!`,
+        embeds: [renderAchievement(putAchievement, true)],
+        ephemeral: true,
+    })
 }
 
 async function handleDelete(interaction: CommandInteraction): Promise<void> {
+    const crystal = interaction.options.get("crystal").value
+    const response = await isapi.delete(`/api/achievement/v1/${crystal}`, {
+        params: {
+            group: interaction.guildId,
+        }
+    })
 
+    await interaction.reply({
+        content: `:wastebasket: Deleted achievement \`${crystal}\`!`,
+        ephemeral: true,
+    })
 }
 
 async function handleView(interaction: CommandInteraction): Promise<void> {
---
+++ src/msgrender.ts
@@ -0,0 +1,57 @@
+import {ColorResolvable, MessageEmbed} from "discord.js"
+import {AchievementFull} from "./types"
+
+const ALLOY_EMOJIS = {
+    "BRONZE": ":third_place:",
+    "SILVER": ":second_place:",
+    "GOLD": ":first_place:",
+}
+
+const ALLOY_NAMES = {
+    "BRONZE": "Bronze",
+    "SILVER": "Silver",
+    "GOLD": "Gold",
+}
+
+const ALLOY_COLORS: {[_: string]: ColorResolvable} = {
+    "BRONZE": "#ff8a3b",
+    "SILVER": "#ccd6dd",
+    "GOLD": "#ffac33",
+}
+
+export function renderAchievement(achievement: AchievementFull, full: boolean): MessageEmbed {
+    let embed = new MessageEmbed()
+
+    embed = embed.setTitle(achievement.name)
+
+    if(achievement.secret) {
+        embed = embed.setDescription(achievement.description.split("\n").map(line => `||${line}||`).join("\n"))
+    }
+    else {
+        embed = embed.setDescription(achievement.description)
+    }
+
+    embed = embed.setColor(ALLOY_COLORS[achievement.alloy])
+
+    if(full) {
+        embed = embed.addField("Crystal", `:crystal_ball: \`${achievement.crystal}\``, true)
+
+        embed = embed.addField("Alloy", `${ALLOY_EMOJIS[achievement.alloy]} ${ALLOY_NAMES[achievement.alloy]}`, true)
+
+        if(achievement.repeatable) {
+            embed = embed.addField("Repeatable", ":repeat_one: Yes", true)
+        }
+        else {
+            embed = embed.addField("Repeatable", ":one: No", true)
+        }
+
+        if(achievement.secret) {
+            embed = embed.addField("Secret", ":key: Yes", true)
+        }
+        else {
+            embed = embed.addField("Secret", ":newspaper: No", true)
+        }
+    }
+
+    return embed
+}
