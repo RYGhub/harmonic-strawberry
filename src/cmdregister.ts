dc2b02b487f5be28d8b837cb993bf4fc29e1bca0
Stefano Pigozzi <me@steffo.eu>
2022-01-07 02:05:30 +0100

    🔧 Add logging to cmdregister.ts

--- src/cmdregister.ts
+++ src/cmdregister.ts
@@ -4,13 +4,21 @@ import {default as cmdList} from "./cmddfn"
 
 
 async function registerGuild(rest: REST, applicationId: Snowflake, guildId: Snowflake): Promise<void> {
+    console.info("Registering commands as **guild** for", applicationId, "in guild", guildId)
+
     await rest.put(Routes.applicationGuildCommands(applicationId, guildId), {
         body: cmdList.map(cmd => cmd.toJSON()),
     })
+
+    console.info("Registration complete!")
 }
 
 async function registerGlobal(rest: REST, applicationId: Snowflake): Promise<void> {
+    console.info("Registering commands as **global** for", applicationId)
+
     await rest.put(Routes.applicationCommands(applicationId), {
         body: cmdList.map(cmd => cmd.toJSON()),
     })
+
+    console.info("Registration complete!")
 }
